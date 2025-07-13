54f6f3ff70a00bf13de6e894b91b814f20044c8b
Ian Kerich <iankerich254@gmail.com>
2025-07-13 19:47:23 +0300

    Cache Property List View

--- properties/views.py
+++ properties/views.py
@@ -6,4 +6,4 @@ from .models import Property
 @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all().values()  # Convert queryset to list of dicts
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse({"data": properties})
